1ee683e825b29732b1ffc03298cf701e86957425
Kei Nomura <mypacecreator@gmail.com>
2017-01-17 20:54:33 +0900

    完了画面

--- front-sample/complete.php
+++ front-sample/complete.php
@@ -1,7 +1,7 @@
 <?php
 
 // 設定ファイルのパスを設定
-$config_path = dirname(__FILE__) . '/' . 'sample_form1.json';
+$config_path = dirname(__FILE__) . '/' . 'ozn-config.json';
 
 
 // Gmail アカウント設定（Gmail SMTP 経由で送信する時のみ）
@@ -42,7 +42,7 @@ $gmail_password = "nNeT7FYANyWtDX";
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 
 // 管理者宛メールタイトル
-$admin_mail_title = 'web：<% customer_name %> 様より、お問い合わせがありました';
+$admin_mail_title = 'web：<% title %>のお問い合わせがありました';
 
 // 管理者宛メールテンプレート
 $admin_mail_body = <<< TEXT
@@ -50,18 +50,22 @@ $admin_mail_body = <<< TEXT
 Webフォームにて <% customer_name %> 様よりお問合せがありました。
 
 - - - - - - - - - - - - - - - - - - - - - - - - - - -
-お名前： <% customer_name %>
-<%% if.customer_kana %%>よみがな： <% customer_kana %><%% endif %%>
-ご住所： 〒<% zip-code %> <% address1 %> <% address2 %>
+<%% if.title %%>お問い合わせ種別： <% title %><%% endif %%>
+<%% if.corporate_name %%>企業名・団体名： <% corporate_name %><%% endif %%>
+<%% if.corporate_kana %%>企業名・団体名フリガナ： <% corporate_kana %><%% endif %%>
+<%% if.customer_name %%>ご担当者様氏名： <% customer_name %><%% endif %%>
+<%% if.customer_kana %%>ご担当者様フリガナ： <% customer_kana %><%% endif %%>
+<%% if.email %%>メールアドレス： <% email %><%% endif %%>
 <%% if.tel %%>電話番号： <% tel %><%% endif %%>
-メールアドレス： <% email %>
+<%% if.tel %%>FAX番号： <% fax %><%% endif %%>
+ご住所： <%% if.zip-code %%>〒<% zip-code %><%% endif %%>
+　　　　<% address1 %> <% address2 %>
 
 - - - - - - - - - - - - - - - - - - - - - - - - - - -
-資料送付： <% materials %>
-配送希望日： <% shipping-date %>
-送付先ご住所： 〒<% shipping-zip-code1 %>-<% shipping-zip-code2 %> <% shipping-address1 %> <% shipping-address2 %>
-
-アンケート： <% survey[] %>
+<%% if.shipping-date %%>ご希望納期： <% shipping-date %><%% endif %%>
+<%% if.survey[] %%>チェック項目： <% survey[] %><%% endif %%>
+<%% if.materials %%>選択項目： <% materials %><%% endif %%>
+<%% if.mail_body %%>お問い合わせ詳細： <% mail_body %><%% endif %%>
 
 TEXT;
 
@@ -70,101 +74,109 @@ TEXT;
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 
 // 自動返信メールタイトル
-$customer_mail_title = '<% customer_name %>さま、お問合せありがとうございます';
+$customer_mail_title = '＊＊＊へのお問合せありがとうございます';
 
 // 自動返信メールテンプレート
 $customer_mail_body = <<< TEXT
 
-下記の通り、承りました。お問合せありがとうございました。
-2〜3営業日以内にお返事いたします。
+<% customer_name %>様
+
+＊＊＊＊＊＊株式会社へのお問い合わせ、ありがとうございます。
+本メールはメールフォームより送信した内容をお知らせする自動返信メールです。
+送信された内容は下記の通りですのでお確かめください。
 
 - - - - - - - - - - - - - - - - - - - - - - - - - - -
-お名前： <% customer_name %>
-<%% if.customer_kana %%>よみがな： <% customer_kana %><%% endif %%>
-ご住所： 〒<% zip-code %> <% address1 %> <% address2 %>
+<%% if.title %%>お問い合わせ種別： <% title %><%% endif %%>
+<%% if.corporate_name %%>企業名・団体名： <% corporate_name %><%% endif %%>
+<%% if.corporate_kana %%>企業名・団体名フリガナ： <% corporate_kana %><%% endif %%>
+<%% if.customer_name %%>ご担当者様氏名： <% customer_name %><%% endif %%>
+<%% if.customer_kana %%>ご担当者様フリガナ： <% customer_kana %><%% endif %%>
+<%% if.email %%>メールアドレス： <% email %><%% endif %%>
 <%% if.tel %%>電話番号： <% tel %><%% endif %%>
-メールアドレス： <% email %>
+<%% if.tel %%>FAX番号： <% fax %><%% endif %%>
+ご住所： <%% if.zip-code %%>〒<% zip-code %><%% endif %%>
+　　　　<% address1 %> <% address2 %>
 
 - - - - - - - - - - - - - - - - - - - - - - - - - - -
-資料送付： <% materials %>
-配送希望日： <% shipping-date %>
-送付先ご住所： 〒<% shipping-zip-code1 %>-<% shipping-zip-code2 %> <% shipping-address1 %> <% shipping-address2 %>
+<%% if.shipping-date %%>ご希望納期： <% shipping-date %><%% endif %%>
+<%% if.survey[] %%>チェック項目： <% survey[] %><%% endif %%>
+<%% if.materials %%>選択項目： <% materials %><%% endif %%>
+<%% if.mail_body %%>お問い合わせ詳細： <% mail_body %><%% endif %%>
 
-アンケート： <% survey[] %>
+
+担当者が内容を確認の上、改めてご連絡いたします。
+
+=======================================================
+会社名
+〒460-0000 住所
+TEL：  FAX： 
+URL: http://
+=======================================================
+
 
 TEXT;
 
 
 // OznForm 実行ファイル読み込み
-require '../../ozn-form.php';
+require '../ozn-form/ozn-form.php';
 
 ?>
 
 
 <!-- 送信完了画面のHTMLをここから下に記述する。特定ページへリダイレクトする場合は省略可。 -->
 
-
-
 <!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Ozn-Form - サンプルフォーム</title>
-    <meta charset="utf-8">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="../css/sample_form_style.css">
-
-    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
+<title>Ozn-Form - サンプルフォーム - STEP03</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width">
+<?php echo $ozn_form_styles; ?>
+<link rel="stylesheet" href="css/ozn-form.css">
+<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+<?php echo $ozn_form_javascript; ?>
+<!--[if lt IE 9]><script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
 </head>
 <body>
 
-<nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Ozn-Form</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li><a href="#">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li class="active"><a href="#contact">サンプルフォーム</a></li>
-            </ul>
-        </div><!--/.nav-collapse -->
-    </div>
-</nav>
+<?php include('./inc/header.php'); ?>
 
 <div class="container">
 
-    <div class="page-header">
-        <h1>
-            お問い合わせ
-            <small>送信完了</small>
-        </h1>
+	<div class="page-header">
+		<h1>お問い合わせ</h1>
+	</div>
 
-        <div class="row">
-            <p class="col-sm-12 text-center">
-                <a href="/document/sample_form/" class="btn btn-success">フォームトップへ戻る</a>
-            </p>
-        </div>
-    </div>
+	<div class="step_bar_box">
+		<ol class="step_bar step3 sp-hide">
+			<li>1. 内容の入力</li>
+			<li>2. 内容確認</li>
+			<li class="current">3. 送信完了</li>
+		</ol>
+		<ol class="step_bar step3 tb-hide pc-hide">
+			<li>入力</li>
+			<li>確認</li>
+			<li class="current">完了</li>
+		</ol>
+	</div>
+
+	<div class="ozn-form-container ozn-form-complete">
+
+		<div class="ozn-form-lead">
+			<h2>送信完了しました</h2>
+			<p>お問い合わせありがとうございました。<br />
+			担当者が内容を確認の上、改めてご連絡いたします。<br />
+			今しばらくお待ちください。</p>
+			<p>&raquo;<a href="/" title="HOME">トップページへ戻る</a></p>
+		</div>
+
+	</div>
+
 </div>
+
+<?php include('./inc/footer.php'); ?>
+
+</body>
+</html>
