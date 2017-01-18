2afcdcabd782379acc8bdf9dffde006052137e98
M.Ichikawa <michikawa@seisys.jp>
2017-01-18 21:41:48 +0900

    郵便番号形式チェックを追加 resolved issue #28

--- release/lib/custom_validation_rules.php
+++ release/lib/custom_validation_rules.php
@@ -10,3 +10,12 @@
 \Valitron\Validator::addRule('tel', function($field, $value, array $params, array $fields) {
     return preg_match("/^[-ー−0-9０-９（）\(\)\s]{9,}$/u", $value) ? true : false;
 }, "は市外局番を含む数字とハイフンの組み合わせで入力してください");
+
+/**
+ * 郵便番号検証
+ *
+ * 数字３桁・４桁・７桁（ハイフン含む）のとき検証OK
+ */
+\Valitron\Validator::addRule('zip', function($field, $value, array $params, array $fields) {
+    return preg_match("/^\d{3}$|^\d{4}$|^\d{3}[\x{30FC}\x{2010}-\x{2015}\x{2212}\x{FF70}-]{0,1}\d{4}$/u", $value) ? true : false;
+}, "の書式が違います。");
