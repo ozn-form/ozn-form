7be0d383297d4d3019a6631d641082d1f10fc648
M.Ichikawa - seisys <michikawa@seisys.jp>
2017-05-09 13:39:24 +0900

    サンプルフォーム調整

--- document/samples/step/complete.php
+++ document/samples/step/complete.php
@@ -111,7 +111,7 @@ require '../../../release/ozn-form.php';
 <html>
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Ozn-Form Sample - シンプル</title>
+    <title>Ozn-Form Sample - ステップ</title>
     <meta charset="utf-8">
     <meta name="description" content="">
     <meta name="author" content="">
@@ -120,7 +120,7 @@ require '../../../release/ozn-form.php';
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="../..//css/style.min.css">
+    <link rel="stylesheet" href="../../css/style.min.css">
 
     <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
@@ -153,9 +153,9 @@ require '../../../release/ozn-form.php';
         </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="/document/samples/simple/">シンプル</a></li>
+                <li class=""><a href="/document/samples/simple/">シンプル</a></li>
                 <li class=""><a href="/document/samples/no_confirm/">確認スキップ</a></li>
-                <li class=""><a href="/document/samples/step/">ステップ</a></li>
+                <li class="active"><a href="/document/samples/step/">ステップ</a></li>
                 <!--<li class="&lt;!&ndash; @@var= home_active &ndash;&gt;"><a href="#about">About</a></li>-->
                 <!--<li class="active"><a href="#contact">サンプルフォーム</a></li>-->
             </ul>
--- html-extend-src/document/pages/samples/step/confirm.php
+++ html-extend-src/document/pages/samples/step/confirm.php
@@ -1,4 +1,4 @@
-<!-- @@master ../../../layout/sample.html {"title": "シンプル", "relative_path": "../../", "simple_active":"active"} -->
+<!-- @@master ../../../layout/sample.html {"title": "シンプル", "relative_path": "../..", "simple_active":"active"} -->
 <!-- @@block php -->
 <?php
 
