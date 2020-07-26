ad7ac6b70ba01fdfa18b486d0485ed2604dc3a6c
M.Ichikawa <masaya.ichikawa@creato-c.jp>
2020-07-26 17:26:30 +0900

    autoloader対応のためcomposer.jsonの場所変更

---
+++ release/ozn-form/test/UnitTests/SampleTest.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once __DIR__ . '/../../release/ozn-form/vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+
+class SampleTest extends TestCase
+{
+    public function testPushAndPop()
+    {
+        $stack = [];
+        $this->assertSame(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertSame('foo', $stack[count($stack)-1]);
+        $this->assertSame(1, count($stack));
+
+        $this->assertSame('foo', array_pop($stack));
+        $this->assertSame(0, count($stack));
+        
+        
+    }
+}
